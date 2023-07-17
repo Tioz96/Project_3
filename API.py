d8aec0a9f151d575158f6af93439d9ac242a52e4
JC Cano <127796306+Tioz96@users.noreply.github.com>
2023-07-16 21:26:29 -0600

    API
    changes

--- API.py
+++ API.py
@@ -1,19 +1,19 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 from pymongo import MongoClient
-import json
 
 app = Flask(__name__)
 
-mongo = MongoClient('mongodb://localhost:27017/')
-db = mongo['F1database']
-collection = db['DataSets']
-@app.route('/api/documentos', methods=['GET'])
-def mostrar_documento():
-    documento = collection.find_one()
-    if documento:
-        return jsonify(documento)
- #   else:
-#      return jsonify({'mensaje': 'No se encontraron documentos'}), 404
+# Configuración de la conexión a la base de datos MongoDB
+client = MongoClient('localhost', 27017)
+db = client.F1database
+collection = db['collection']
+
+# Ruta para obtener los datos de la base de datos y pasarlos al archivo HTML
+@app.route('/')
+def index():
+    data = list(collection.find())  # Obtiene los primeros tres documentos de la colección
+
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run()
