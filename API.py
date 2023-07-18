18e1eca3f099bf3ae359c0799d39b427cb8d0b82
Tioz96 <127796306+Tioz96@users.noreply.github.com>
2023-07-17 19:15:43 -0600

    Update API.py

---
+++ API.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# Configuración de la conexión a la base de datos MongoDB
+client = MongoClient('localhost', 27017)
+db = client.F1database
+collection = db['collection']
+
+# Ruta para obtener los datos de la base de datos y pasarlos al archivo HTML
+@app.route('/')
+def index():
+    data = list(collection.find().limit(3))  # Obtiene los primeros tres documentos de la colección
+
+    return render_template('index.html', data=data)
+
+if __name__ == '__main__':
+    app.run()
